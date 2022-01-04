a25a2881fec33b58e9a1fc18d9e6bc9d2d0206e8
DeathTrigger <96653366+FaiyazShahriar98@users.noreply.github.com>
2022-01-04 08:57:50 +0600

    Add files via upload

---
+++ Project/parts.php
@@ -0,0 +1,72 @@
+<?php include('db-conn.php'); ?>
+
+
+
+<section class="section" id="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3"> <!-- CHECBK later -->
+                <div class="section-heading">
+                    <h2>Specific <em>Parts Servicing</em></h2>
+                    <img src="assets/images/line-dec.png" alt="waves">
+                
+                </div>
+            </div>
+            <div class="col-lg<-6">
+                <ul class="features-items">
+                    <li class="feature-item">
+                        <div class="left-icon">
+                            <img src="assets/images/icons/ac.png">
+                        </div>
+                        <div class="right-content">
+                            <h4>Air Conditioning Systems</h4>
+                            <p>Air conditioning not only cools it down, but also reduces the moisture content, or humidity. We'll stick to the main points or a car's air conditioning and the main parts used and a few hints to keep the air-con system running properly.</p>
+                            <a href="#" class="text-button">Discover More</a>
+                        </div>
+                    </li>
+                    <li class="feature-item">
+                        <div class="left-icon">
+                            <img src="assets/images/icons/abs.png">
+                        </div>
+                        <div class="right-content">
+                            <h4>Antilock Brake Repair</h4>
+                            <p>Since different manufacturers have their own versions of ABS their specifications and part names may differ. If you're having a problem with the ABS on your vehicle you should always refer to the specific service and repair manuals for your vehicle.</p>
+                            <a href="#" class="text-button">Discover More</a>
+                        </div>
+                    </li>
+                    <li class="feature-item">
+                        <div class="left-icon">
+                            <img src="assets/images/icons/hl.png">
+                        </div>
+                        <div class="right-content">
+                            <h4>Headlights & Headlamps</h4>
+                            <p>A headlamp is a lamp attached to the front of a vehicle to light the road ahead. The U.S. National Highway Traffic Safety Administration states that nearly half of all traffic-related fatalities occur in the dark, despite only 25% of traffic travelling during darkness.</p>
+                            <a href="#" class="text-button">Discover More</a>
+                        </div>
+                    </li>
+                    <li class="feature-item">
+                        <div class="left-icon">
+                            <img src="assets/images/alt.png">
+                        </div>
+                        <div class="right-content">
+                            <h4>Alternators & Starters</h4>
+                            <p>When the alternator is no longer generating power, your vehicle will slowly run down the battery until there is not enough power to fire the spark plugs, at which time your engine dies. </p>
+                            <a href="#" class="text-button">Discover More</a>
+                        </div>
+                    </li>
+                    <li class="feature-item">
+                        <div class="left-icon">
+                            <img src="assets/images/icons/dent.png">
+                        </div>
+                        <div class="right-content">
+                            <h4>Power Window Repair</h4>
+                            <p>Power windows or electric windows are automobile windows which can be raised and lowered by depressing a button or switch, as opposed to using a hand-turned crank handle. </p>
+                            <a href="#" class="text-button">Discover More</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</section>
